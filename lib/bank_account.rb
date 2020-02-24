cd78bb929d3839dd7afec2cdc9135eb73b9cb6b8
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-02-24 23:47:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,3 @@
-class BankAccount
-    
-    attr_accessor :name
-    
-    def initialize(name, bank_account)
-      @name = name
-      @bank_account = bank_account
-      @balance = 1000 
-      @status = 'open'
-    end
-end
 
 class BankAccount
   # code here
