c9a9280978581de6e9f89d8ed9e0ba226cd3443d
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-02-24 23:47:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,3 +9,36 @@ class BankAccount
       @status = 'open'
     end
 end
+
+class BankAccount
+  # code here
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your Balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+end
