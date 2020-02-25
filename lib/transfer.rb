a1dc93a9154df80f68f493591f06e3a65fdb83fb
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-02-25 00:30:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,9 +8,11 @@ class Transfer
     @amount = amount
     @status = "pending"
   end
-
+ 
+    def valid 
     @sender.valid? && @receiver.valid? ? true : false
-
+  end 
+  
   def execute_transaction
     if @sender.balance < @amount
       @status = "rejected"
